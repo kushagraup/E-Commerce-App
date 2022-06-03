52008ccf519a947b7258d369a23b0ee2ac34299c
Kushagra Mishra <kushagra@Kushagras-MacBook-Pro.local>
2022-06-03 17:32:28 +0530

    signup/signin page ui

---
+++ online_shop/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:online_shop/constants/global_variables.dart';
+import 'package:online_shop/features/auth/screens/auth_screen.dart';
+import 'package:online_shop/router.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'E-Commerce-App',
+      theme: ThemeData(
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        colorScheme: const ColorScheme.light(
+          primary: GlobalVariables.secondaryColor,
+        ),
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          iconTheme: IconThemeData(
+            color: Colors.black,
+          ),
+        ),
+      ),
+      onGenerateRoute: (settings) => generateRoute(settings),
+      home: const AuthScreen(),
+    );
+  }
+}
